749df180294c20654d45d82c2157448e0de5b9e2
Jacob Ogren <jogren0044@gmail.com>
2019-11-18 10:35:13 -0700

    Add Header component with basic styling

---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="Header_container">
+    <h1>jacob <br />
+    <span class="header_bold">ogren</span></h1>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'  
+}
+</script>
+
+
+<style scoped>
+.Header_container {
+  display: flex;
+  align-items: center;
+  width: 50%;
+}
+
+h1 {
+  padding: 10px;
+}
+
+.header_bold {
+  font-weight: 700;
+  padding: 10px;
+}
+
+hr {
+  width: 100%;
+  height: 0;
+}
+</style>
