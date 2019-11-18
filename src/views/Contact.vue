9293013562c07e58b06a7772faf601e3d608128c
Jacob Ogren <jogren0044@gmail.com>
2019-11-18 11:25:04 -0700

    Add contact component

---
+++ src/views/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="content-container contact-container">
+    <h1>This is an contact page</h1>
+    <p>Contact Jacob</p>
+    <p>LinkedIn</p>
+    <p>GitHub</p>
+    <p>Portfolio</p>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+.contact-container {
+  background-color: #ffd60c;
+}
+
+</style>
