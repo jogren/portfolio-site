29a82176da385730eed05343b4b7a74d7bf470eb
Jacob Ogren <jogren0044@gmail.com>
2019-11-20 16:02:32 -0700

    Update routes to link to portfolio and resume

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <main id="app">
     <div id="nav">
       <router-link to="/">About</router-link> |
-      <router-link to="/work">Work</router-link> |
-      <router-link to="/resume">Resume</router-link> |
+      <a href="https://alumni.turing.io/alumni/jacob-ogren" target="_blank">Work</a> |
+      <a href="https://alumni.turing.io/sites/default/files/resumes/Resume_Jacob_Ogren.pdf" target="_blank">Resume</a> |
       <router-link to="/contact">Contact</router-link>
     </div>
     <Header />
@@ -33,6 +33,7 @@ body {
   height: 100vh;
   width: 100%;
   margin: 0;
+  background-color: #3e3b39;
 }
 
 #app {
@@ -44,20 +45,24 @@ body {
   position: relative;
   margin: 0 auto;
   display: flex;
+  border-bottom: #fff 1px solid;
 }
 
 #nav {
   padding: 30px 0px;
   width: 50%;
   margin: auto 0;
+  border-bottom: #fff 1px solid;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #fff;
+  opacity: 0.5;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #fff;
+  opacity: 1;
 }
 </style>
