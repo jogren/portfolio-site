cbd83f7f1088b3fc49e9cc10236a9dc1af55f746
Jacob Ogren <jogren0044@gmail.com>
2019-11-21 13:46:50 -0700

    Make all contact links clickable and add path

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="content-container contact-container">
     <h1>get in touch</h1>
-    <p><img src="../../public/phone-solid.svg"/> 612.655.6411</p>
-    <p><img src="../../public/envelope-solid.svg"/> jogren0044@gmail.com</p>
-    <p><img src="../../public/linkedin-in-brands.svg"/> linkedin.com/in/jacobogren/</p>
-    <p><img src="../../public/github-brands.svg"/> github.com/jogren</p>
-    <p><img src="../../public/graduation-cap-solid.svg"/> alumni.turing.io/alumni/jacob-ogren</p>
+    <a href="tel:+16126556411"><img src="../../public/phone-solid.svg"/> 612.655.6411</a>
+    <a href="mailto:jogren0044@gmail.com"><img src="../../public/envelope-solid.svg"/> jogren0044@gmail.com</a>
+    <a href="https://www.linkedin.com/in/jacobogren/" target="_blank"><img src="../../public/linkedin-in-brands.svg"/> linkedin.com/in/jacobogren/</a>
+    <a href="https://github.com/jogren" target="_blank"><img src="../../public/github-brands.svg"/> github.com/jogren</a>
+    <a href="https://alumni.turing.io/alumni/jacob-ogren" target="_blank"><img src="../../public/graduation-cap-solid.svg"/> alumni.turing.io/alumni/jacob-ogren</a>
   </div>
 </template>
 
@@ -19,6 +19,13 @@ export default {
 <style scoped>
 .contact-container {
   background-color: #ffd60c;
+}
+
+.contact-container a {
+  text-decoration: none;
+  display: block;
+  color: #3e3b39;
+  margin-bottom: 10px;
 }
 
 h1 {
