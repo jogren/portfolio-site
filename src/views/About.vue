1ada42baf43bc2f0372951d81cb8a0e651f0e687
Jacob Ogren <jogren0044@gmail.com>
2019-12-03 17:50:09 -0600

    Update styling for About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="content-container about-container">
-    <div class="img-cropper">
+    <header class="img-header">
+      <div>
+        <h3>Software Engineer</h3>
+      </div>
       <img src="../../public/JacobOgren.jpg"/>
+      <div>
+        <h3>Teacher & Mentor</h3>
+      </div>
+    </header>
+    <div class="list-container">  
+      <article>
+        <ul style="list-style-type:square;">
+          <li>UI/UX Enthusiast</li>
+          <li>Creative problem-solver and lover of learning</li>
+          <li>Hands-on experience building intuitive and accessible applications</li>
+        </ul>
+      </article>
+      <article>
+        <ul style="list-style-type:square;">
+          <li>Focused on teaching front-end development to new engineers</li>
+          <li>Passionate about blending professional and technical skills</li>
+        </ul>
+      </article>
     </div>
-    <ul style="list-style-type:square;">
-      <li>Full-Stack Software Engineer and UI/UX Enthusiast</li>
-      <li>Self-starter, problem-solver, and lover of learning</li>
-      <li>Hands-on experience building intuitive and accessible applications</li>
-      <li>Teacher and Mentor</li>
-      <li>Aspiring to join a team that values hard work, collaboration, and developer growth</li>
-    </ul>
   </div>
 </template>
 
@@ -20,64 +34,85 @@ export default {
 </script>
 
 <style>
-.content-container {
-  padding: 50px;
-  margin-top: 10%;
-  width: 50%;
-  animation: scale-up 0.7s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
-}
-
-.about-container {
-  background-color: #0ce9ff;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.about-container img {
-  object-fit: cover;
-  object-position: 100% 25%;
-  width: 120px;
-  height: 120px;
-  border-radius: 50%;
-}
-
-.about-container ul li {
-  margin-bottom: 12px;
-}
-
-@keyframes scale-up {
-  0% { 
-    transform: scaleY(0.1);
-    transform-origin: 0% 100%; 
-  }
-  100% {
-    transform: scaleY(1);
-    transform-origin: 0% 100%;
-   }
-}
-
-@media only screen and (max-width: 880px) {
   .content-container {
-    width: 100%;
-    margin-top: 0px;
-    position: absolute;
-    top: 200px;
+    margin-top: 10%;
+    width: 50%;
+    min-height: 400px;
+    animation: scale-up 0.7s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
   }
-}
 
-@media only screen and (max-width: 570px) {
-  .content-container {
-    min-height: fit-content;
-  }
-  
   .about-container {
-    padding: 20px 10px;
+    background-color: #0ce9ff;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 25px 15px;
+  }
+
+  .about-container img {
+    object-fit: cover;
+    object-position: 100% 25%;
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+  }
+
+  .img-header {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+  }
+
+  .img-header div {
+    width: 100px;
+  }
+
+  .list-container {
+    display: flex;
+  }
+
+  h3 {
+    text-align: center;
+    font-weight: 500;
+    font-size: 22px;
   }
 
   .about-container ul li {
-    font-size: 12px;
+    margin-bottom: 12px;
   }
-}
 
+  @keyframes scale-up {
+    0% { 
+      transform: scaleY(0.1);
+      transform-origin: 0% 100%; 
+    }
+    100% {
+      transform: scaleY(1);
+      transform-origin: 0% 100%;
+    }
+  }
+
+  @media only screen and (max-width: 880px) {
+    .content-container {
+      width: 100%;
+      margin-top: 0px;
+      position: absolute;
+      top: 200px;
+    }
+  }
+
+  @media only screen and (max-width: 570px) {
+    .content-container {
+      min-height: fit-content;
+    }
+    
+    .about-container {
+      padding: 20px 10px;
+    }
+
+    .about-container ul li {
+      font-size: 12px;
+    }
+  }
 </style>
